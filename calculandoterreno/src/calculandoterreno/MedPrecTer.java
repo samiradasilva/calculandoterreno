cbbac34402e3d9a6d24437f6c36308c2f5b8bfdc
Samira <tisamirasilva@gmail.com>
2021-03-01 16:51:42 -0300

    criado projeto metragem de terreno

---
+++ calculandoterreno/src/calculandoterreno/MedPrecTer.java
@@ -0,0 +1,26 @@
+package calculandoterreno;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class MedPrecTer { 
+
+	public static void main(String[] args) {
+
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+
+		double largura = sc.nextDouble();
+		double comprimento = sc.nextDouble();
+		double MetroQuadrado = sc.nextDouble();
+		
+		double area = largura * comprimento;
+		double preco = area * MetroQuadrado;
+		
+		System.out.printf("AREA = %.2f%n", area);
+		System.out.printf("PRECO = %.2f%n", preco);
+		
+		sc.close();
+	}
+
+}
